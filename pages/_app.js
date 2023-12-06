3ea44cb4d3132a6174d3ca5617ca048a12916269
calvinmaighan <calvinmaighan@gmail.com>
2023-12-06 13:29:47 +0100

    Added Klaviyo script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          async
+          type="text/javascript"
+          src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=VUN8Ba"
+        ></script>
+      </Head>
       <GoogleAnalytics trackPageViews gaMeasurementId={"G-ZTF0DHQ9JD"} />
       <Component {...pageProps} />
     </>
